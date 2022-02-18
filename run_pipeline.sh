78312dfe3478978bc2ec97fa85cc324c4f9027a9
Max Vasterd <max.vasterd@paconsulting.com>
2022-02-18 14:52:45 +0100

    exit on any errored step

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -13,12 +13,14 @@ exit_on_error () {
 
 echo "Start preprocessing the data..."
 PREPROCESSED=$(cat data/input.json | curl --show-error --fail -s -H "Content-Type: application/json" -X POST -d @- http://127.0.0.1:8000/preprocess)
-echo
 exit_on_error $?
+echo
 write_metadata "PREPROCESSING" $PREPROCESSED
 echo
 echo "Start splitting the data..."
 SPLITTED=$(echo $PREPROCESSED | jq .out | curl --show-error --fail -s -H "Content-Type: application/json" -X POST -d @- http://localhost:8001/split)
+exit_on_error $?
+
 TRAIN_DATA=$(echo $SPLITTED | jq .out.train)
 TEST_DATA=$(echo $SPLITTED | jq .out.test)
 echo
@@ -26,6 +28,8 @@ write_metadata "SPLITTING" $SPLITTED
 echo
 echo "Start training the using the training data..."
 TRAINED=$(echo $TRAIN_DATA | curl --show-error --fail -s -H "Content-Type: application/json" -X POST -d @- http://localhost:8002/train)
+exit_on_error $?
+
 CV_RESULT=$(echo $TRAINED | jq .out.cv_result)
 MODEL=$(echo $TRAINED | jq .out.model)
 echo
@@ -33,6 +37,8 @@ write_metadata "TRAINING" $TRAINED
 echo
 echo "Start validating using the test data, and model..."
 VALIDATION=$(echo "{\"model\": $MODEL, \"test_data\": $TEST_DATA}" | curl --show-error --fail -s -H "Content-Type: application/json" -X POST -d @- http://localhost:8003/validate?show_plot=$SHOW_PLOT)
+exit_on_error $?
+
 METRICS_DICT=$(echo $VALIDATION | jq .out.metrics_dict)
 write_metadata "VALIDATING" $VALIDATION
 echo
